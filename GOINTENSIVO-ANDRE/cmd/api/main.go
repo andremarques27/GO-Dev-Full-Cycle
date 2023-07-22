f19d258a0abb11254c47140ce31107c263745d23
Andre Marques <95247935+andremarques27@users.noreply.github.com>
2023-07-22 15:23:49 -0300

    aula 3

---
+++ GOINTENSIVO-ANDRE/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/devfullcycle/go-intensivo-andre/internal/entity"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/order", Order)
+	http.ListenAndServe(":8888", r)
+}
+
+func Order(w http.ResponseWriter, r *http.Request) {
+	order, err := entity.NewOrder("123", 1000, 10)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+	order.CalculateFinalPrice()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(order)
+}
